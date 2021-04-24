4629b35c1667697e594506148628d2fb7162e5f8
TediashviliGiorgi <personalmailgt@yahoo.com>
2021-04-24 23:09:27 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder</h2>;
+  const [people, setPeople] = useState(data)
+  return (
+  
+    <main>
+      <section className = 'container'>
+        <strong><h2>Birthday reminder</h2></strong>
+        <h3>{people.length} beirthdays today</h3>
+        <List people = {people}/>
+        <button onClick = {() => setPeople([])}> Clear all</button>
+      </section>
+    </main>
+  )
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
   return (
     <>
+    {people.map((person) =>{
+      return(
+        <article key = {person.id} className = 'person'>
+          <img src = {person.image} alt = 'personImg' ></img>
+          <div>
+            <h4>{person.name}</h4>
+            <p>{person.age} years old</p>
+          </div>
+        </article>
+      )
+    })}
       <h2>list component</h2>
     </>
   );
